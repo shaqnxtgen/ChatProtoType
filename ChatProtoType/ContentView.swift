cb85e1ce0733baab1708cb2f64e32f06fa56eb85
Shaquille Nelson <shaqatak96@gmail.com>
2025-09-18 18:01:10 -0400

    Day 2: Added button and state to update message

--- ChatProtoType/ContentView.swift
+++ ChatProtoType/ContentView.swift
@@ -5,34 +5,30 @@
 //  Created by Shaquille Nelson on 9/18/25.
 //
 
-import SwiftUI
+import SwiftUI  // Swift gives us UI components
 
 struct ContentView: View {
+    // MARK: - State
+    @State private var message = "Knock, knock!"
+    // @State = special SwiftUI property wrapper
+    // "message" will be watched for changes
+    // If it changes, SwiftUI will re-render the body
+    
+    
     var body: some View {
-        VStack(spacing: 25) {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-            Text("Knock, knock!")
-                .padding()
-                .background(Color.blue, in: RoundedRectangle(cornerRadius: 8))
-            Text("Who's there?")
-                .padding()
-                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
-            Text("Mew")
-                .padding()
-                .background(Color.blue, in: RoundedRectangle(cornerRadius: 8))
-            Text("Mew Who?")
-                .padding()
-                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
-            Text("Mew-Two!!")
-                .padding()
-                .background(Color.blue, in: RoundedRectangle(cornerRadius: 8))
-            Text("That's right Pokémon")
-                .bold()
-                .padding()
-                .background(Color.gray, in: RoundedRectangle(cornerRadius: 8))
-            
+        VStack(spacing: 16) {   // spacing = space between elements
+                // MARK: - Display current message
+                Text(message)   // Show the current value of "message"
+                    .padding()
+                    .background(Color.blue, in: RoundedRectangle(cornerRadius: 8))
+                    .foregroundColor(.white)   // text color
+            // MARK: - Button
+                Button("Tap me") {
+                         message = "Who's there?"  // Updates the state
+                     }
+                     .padding()
+                     .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+                     .foregroundColor(.white)
         }
         .padding()
     }
